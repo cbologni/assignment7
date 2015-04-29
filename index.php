d8a2ca5854ea1d0d54282128a12ebc0a0ac9c81f
cbologni <cbologni@uvm.edu>
2015-04-29 15:06:23 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<!-- **** you need to put in y our own title below -->
+  <title>Christine E. Bolognino</title>
+	 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+
+<!-- **** you need to in your own description in the content below -->	
+	<meta name="description" content="A UVM Student's Guide to Local Hiking" >
+
+<!-- **** you need to in your name in the content below -->
+	<meta name="author" content="Christine E. Bolognino" >
+
+<!-- **** the href below is the name of your style sheet that will define the
+look for your resume. -->
+  
+<link rel="stylesheet" href="index.css" type="text/css" />
+  
+
+</head>
+
+<body>
+  
+  <nav class="top-right-menu">
+		<ul>
+			
+				<li  class="current"><a href="/">Home</a></li>
+			
+				<li ><a href="/page1">Directory</a></li>
+			
+				<li ><a href="/page2">Resources</a></li>
+			
+				<li ><a href="/page3">Join The Outing Club</a></li>
+			
+		</ul>
+		
+		
+  </nav>
+  
+  <header>
+  
+      <center>
+      <h1>LoVermont Hiking</h1>
+       <h2>A UVM Student's Guide to Hiking the Burlington Area</h2>
+      </center>
+  
+  </header>
+  
+
+  <img id="background-image" width="1280" height="1400" alt="background image" class="bg_stretch" src="trees.jpg">
+
+<div class="content">
+  
+  
+  <div id="container">
+
+      <div id="leftcol">
+          <h3><a href="https://cbologni.w3.uvm.edu/cs142/assignment7/popular.php" class="cmn-t-border-radius">The Most Popular</a></h3>
+              
+                
+                <h3><a href="https://cbologni.w3.uvm.edu/cs142/assignment7/easiest.php" class="cmn-t-border-radius">The Easiest</a></h3>
+                
+      </div>
+  
+      <div id="rightcol" class="floatright">
+
+          <h3><a href="https://cbologni.w3.uvm.edu/cs142/assignment7/closest.php" class="cmn-t-border-radius">The Closest</a></h3>
+              
+                
+                <h3><a href="https://cbologni.w3.uvm.edu/cs142/assignment7/rewarding.php" class="cmn-t-border-radius">The Most Rewarding</a></h3>
+      </div>
+   </div>
+  
+		 
+  <footer></footer>
+
+</body>
+</html>
